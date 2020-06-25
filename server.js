19a6d6e0ee9f4e61e1f79c7843cb60c24281fabd
cestmarcel <25566817+cestmarcel@users.noreply.github.com>
2020-06-24 21:54:20 -0400

    Added html content and styling for burger page

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 // Dependencies
 var mysql = require("mysql");
 require('dotenv').config();
+var express = require("express");
+var exphbs = require("express-handlebars");
 
-// Setting up connection
+// Create an instance of the express app
+var app = express();
+app.use(express.static('public'));
+
+// Set the port of our application
+var PORT = process.env.PORT || 8080;
+
+// Setting up database connection
 var connection = mysql.createConnection({
     host     : process.env.DB_HOST,
     user     : process.env.DB_USER,
@@ -12,6 +21,23 @@ var connection = mysql.createConnection({
 
 connection.connect(function(err) {
     if (err) throw err;
-    console.log("Connected to the database! Starting employee tracker...");
-    afterConnection();
+    console.log("Connected to the burger database!");
 });
+
+// Set default templating engine
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Routes
+app.get("/", function(req, response) {
+    connection.query("SELECT * FROM items;", function(err, res) {
+        if (err) throw err;
+        console.log(res);
+        response.render("index", {burgers: res});
+      });
+});
+
+// Start the server
+app.listen(PORT, function() {
+    console.log("Server listening on: http://localhost:" + PORT);
+  });
